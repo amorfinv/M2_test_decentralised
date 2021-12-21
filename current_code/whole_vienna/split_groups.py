3dbd6efb3962bc7eb0ce88be7e557af37c13c7c3
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2021-12-21 19:55:33 +0100

    update splitting

--- current_code/whole_vienna/split_groups.py
+++ current_code/whole_vienna/split_groups.py
@@ -36,12 +36,12 @@ def main():
 
     ox.save_graph_geopackage(G, filepath=path.join(gis_data_path, 'finalized_graph.gpkg'), directed=True)
 
-def flow_control_split(edges, average_length=300, ignore_threshold=200):
+def flow_control_split(edges, average_length=300, ignore_threshold=200, merge_threshold=190):
     """
     Code takes stroke groups larger than threshold and splits them to be the average length
 
-    At the end code will check if any split groups are below the ignore_threshold. It will then merge it with the adjacent 
-    split group of the merge. If it is at the end of the stroke group then it
+    At the end code will check if any split groups are below the merge_threshold. It will then merge it with the adjacent 
+    split group of the merge.
     """
     # make split threshold 1.5 times the average length
     split_threshold = average_length * 1.667
@@ -68,7 +68,7 @@ def flow_control_split(edges, average_length=300, ignore_threshold=200):
         length_stroke = stroke_gdf['length'].sum()
 
         # get the stroke indices
-        stroke_edge_indices, length_edges_list = order_stroke_group_edges(stroke_gdf)
+        stroke_edge_indices, length_edges_list = order_group_edges(stroke_gdf)
 
         # get potential number of splits
         num_splits = round(length_stroke / average_length)
@@ -278,7 +278,6 @@ def flow_control_split(edges, average_length=300, ignore_threshold=200):
                 # increase the flow group counter
                 flow_group_counter += 1
 
-    # edges_gdf['flow_group'] = np.zeros(len(edges_gdf), dtype=np.int64)
     edges_gdf['flow_group'] = np.ones(len(edges_gdf), dtype=np.int64)*(-1)
 
     # loop through dictionary
@@ -290,6 +289,121 @@ def flow_control_split(edges, average_length=300, ignore_threshold=200):
         for index in edge_list:
             # add the flow group to the dataframe
             edges_gdf.loc[index, 'flow_group'] = int(key)
+    
+    # find any flow groups less than merge_threshold
+    groups_to_merge = []
+    # loop through the flow_group_dict_keys
+    for key in flow_group_dict.keys():
+
+        # skip group 0 because we don't care about it
+        if key == 0:
+            continue
+
+        # get the length of the flow group
+        edges_in_flow_group = flow_group_dict[key]
+        length_flow_group = edges_gdf.loc[edges_in_flow_group, 'length'].sum()
+
+        # get the stroke group it belongs to
+        stroke_group = edges_gdf.loc[edges_in_flow_group, 'stroke_group'].iloc[0]
+
+        if length_flow_group <= merge_threshold:
+            groups_to_merge.append((key, stroke_group))
+
+    # TODO: small bug merge the groups
+    for group in groups_to_merge:
+        # get the flow group
+        flow_group = group[0]
+
+        # get the stroke group
+        stroke_group = group[1]
+
+        # get the edges in the flow group
+        edges_in_flow_group = flow_group_dict[flow_group]
+
+        # get the flow gdf
+        flow_gdf = edges_gdf.loc[edges_gdf['flow_group'] == flow_group]
+
+        # get the flow indices in order
+        flow_edge_indices, length_edges_list = order_group_edges(flow_gdf)
+        first_node_flow = flow_edge_indices[0][0]
+        last_node_flow = flow_edge_indices[-1][1]
+
+        # get the edges in the edge_gdf that belong to the stroke group
+        stroke_gdf = edges_gdf.loc[edges_gdf['stroke_group'] == stroke_group]
+
+        # get the stroke edge indices in order
+        stroke_edge_indices, length_edges_list = order_group_edges(stroke_gdf)
+
+        # convert stroke edge indices to a list
+        nodes_in_stroke = np.array([item for t in stroke_edge_indices for item in t])
+
+        # get a count of how many occurences each node has in the nodes_in_stroke numpy array
+        v, c = np.unique(nodes_in_stroke, return_counts=True)
+
+        # now check how many times the first_node_flow and last_node_flow occur in the nodes_in_stroke
+        first_node_count = c[v == first_node_flow][0]
+        last_node_count = c[v == last_node_flow][0]
+
+        # if the first_node_count = 2 and last_node_count = 1 then the flow group is at the end of the stroke
+
+        if first_node_count == 2 and last_node_count == 1:
+            # check in stroke_edge_indices for the appeance of first_node_flow in the second poisition of the tuple
+            edge_index = [x for x in stroke_edge_indices if first_node_flow == x[1]][0]
+            preceding_flow_group = edges_gdf.loc[edge_index, 'flow_group'].iloc[0]
+
+            # merge the flow groups
+            flow_group_dict[preceding_flow_group] = flow_group_dict[preceding_flow_group] + flow_group_dict[flow_group]
+
+        # if the first_node_count = 1 and last_node_count = 2 then the flow group is at the start of the stroke
+        elif first_node_count == 1 and last_node_count == 2:
+            # check in stroke_edge_indices for the appeance of the last_node_flow in the first poisition of the tuple
+            edge_index = [x for x in stroke_edge_indices if last_node_flow == x[0]][0]
+            following_flow_group = edges_gdf.loc[edge_index, 'flow_group'].iloc[0]
+
+            # merge the flow groups
+            flow_group_dict[following_flow_group] = flow_group_dict[following_flow_group] + flow_group_dict[flow_group]
+        
+        # if the first_node_count = 2 and last_node_count = 2 then the flow group is in the middle of the stroke
+        elif first_node_count == 2 and last_node_count == 2:
+            # check in stroke_edge_indices for the appeance of first_node_flow in the second poisition of the tuple
+            # check in stroke_edge_indices for the appeance of the last_node_flow in the first poisition of the tuple
+            # get both lengths of these flow groups and merge the current to the smaller one
+            edge_index = [x for x in stroke_edge_indices if first_node_flow == x[1]][0]
+            preceding_flow_group = edges_gdf.loc[edge_index, 'flow_group'].iloc[0]
+
+            edge_index = [x for x in stroke_edge_indices if last_node_flow == x[0]][0]
+            following_flow_group = edges_gdf.loc[edge_index, 'flow_group'].iloc[0]
+
+            # check the lengths of the preceding and following flow groups
+            length_preceding_flow_group = edges_gdf.loc[flow_group_dict[preceding_flow_group], 'length'].sum()
+            length_following_flow_group = edges_gdf.loc[flow_group_dict[following_flow_group], 'length'].sum()
+
+            # merge the flow groups
+            if length_preceding_flow_group < length_following_flow_group:
+                flow_group_dict[preceding_flow_group] = flow_group_dict[preceding_flow_group] + flow_group_dict[flow_group]
+            else:
+                flow_group_dict[following_flow_group] = flow_group_dict[following_flow_group] + flow_group_dict[flow_group]
+
+    # loop through dictionary again to get the new flow groups
+    # to ensure they are in order restart the count of the keys
+    # TODO: for loop could fail if it doesn't start with zero
+    new_key = 0
+    for key in flow_group_dict.keys():
+
+        if key == 0:
+            if not new_key == 0:
+                print('FIX THE CODE!!!, see todo above')
+
+        # get the list of edges
+        edge_list = flow_group_dict[key]
+
+        # loop through the edges in the list
+        for index in edge_list:
+            # add the flow group to the dataframe
+            edges_gdf.loc[index, 'flow_group'] = new_key
+        
+        # increment the key
+        new_key += 1
 
     return edges_gdf
 
@@ -400,7 +514,7 @@ def find_split_indices(stroke_edge_indices, length_edges_list, approximate_lengt
 
     return best_split_choice_index
 
-def order_stroke_group_edges(stroke_gdf):
+def order_group_edges(stroke_gdf):
     # put indices in correct order
     stroke_indices = stroke_gdf.index.to_list()
 
@@ -441,189 +555,6 @@ def order_stroke_group_edges(stroke_gdf):
     lengths_edges_stroke = [length_edges_in_stroke[(stroke_index[0], stroke_index[1], 0)] for stroke_index in stroke_indices_ordered]
 
     return stroke_indices_ordered, lengths_edges_stroke
-
-def flow_control_split2(edges, average_length=300, ignore_threshold=200):
-    """
-    Code takes stroke groups larger than threshold meters and splits them so they around
-    """
-    # make split threshold 1.5 times the average length
-    split_threshold = average_length * 1.5
-
-    # make a copy
-    edges_gdf = edges.copy()
-    
-    stroke_length = {}
-
-    # read json file with stroke group lengths
-    with open(path.join('stroke_length.json'), 'r') as f:
-        stroke_length = json.load(f)
-    flow_group_dict = {}
-    flow_group_counter = 1
-
-    # for loop to go through
-    flow_group_dict[0] = []
-
-    for stroke in stroke_length.keys():
-        length_stroke = stroke_length[stroke]
-
-        # create a dataframe with only the stroke group
-        stroke_gdf = edges_gdf.loc[edges_gdf['stroke_group'] == stroke]
-
-        ###########################################
-        # put indices in correct order
-        stroke_indices = stroke_gdf.index.to_list()
-
-        # remove the zero from the tuple
-        stroke_indices = [(stroke_index[0], stroke_index[1]) for stroke_index in stroke_indices]
-        stroke_indices_list = np.array([item for t in stroke_indices for item in t])
-
-        # find which nodes appear once
-        v,c = np.unique(stroke_indices_list, return_counts=True)
-        unique_indices = v[c == 1]
-
-        # find the first node of the stroke_indices
-        for idx, stroke_index in enumerate(stroke_indices):
-            if stroke_index[0] in unique_indices:
-                first_node = stroke_index[0]
-                index_first_edge = idx
-                break
-        
-        node_to_find = stroke_indices[index_first_edge][1]
-        stroke_indices_ordered = [stroke_indices[index_first_edge]]
-        stroke_indices_check = stroke_indices.copy()
-        # remove the first edge
-        stroke_indices.remove(stroke_indices[index_first_edge])
-
-        while stroke_indices:
-            # find the node in the stroke_indices
-            for idx, stroke_index in enumerate(stroke_indices_check):
-                if stroke_index[0] == node_to_find:
-                    node_to_find = stroke_index[1]
-                    stroke_indices_ordered.append(stroke_index)
-                    break
-            # remove the node from the stroke_indices
-            stroke_indices.remove(stroke_indices_check[idx])
-        
-        ###########################################
-
-        # if the length is less than the ignore threshold, then ignore
-        if length_stroke < ignore_threshold:
-
-            # these will get a flow group of 0
-            for index, edge in stroke_gdf.iterrows():
-                flow_group_dict[0].append(index)
-
-            continue
-    
-        
-        # if the length is greater than the split threshold, then split the group
-        if length_stroke > split_threshold:
-            # check how many splits to do
-            num_splits = round(length_stroke / average_length)
-
-            # set length counter to 0
-            length_counter = 0
-
-            # get a counter for split
-            split_counter = 0
-
-            # loop through the stroke edges
-            edge_list = []
-            for edge_index in stroke_indices_ordered:
-                # get the actual index
-                index = (edge_index[0], edge_index[1], 0)
-
-                edge_length = edges_gdf.loc[index, 'length']
-                
-                # if the length counter is greater than the average length, then split
-                if length_counter + edge_length > average_length:
-
-                    if length_stroke - (length_counter + edge_length) < 100:
-                        a = 1
-                    elif length_counter + edge_length < split_threshold:
-
-                        edge_list.append(index)
-
-                        # get the new stroke group
-                        new_stroke = flow_group_counter
-
-                        # add the new stroke to the dictionary
-                        flow_group_dict[new_stroke] = edge_list
-
-                        length_stroke = length_stroke - (length_counter + edge_length)
-
-                        # extend flow group counter
-                        flow_group_counter += 1
-
-                        # reset the length counter
-                        length_counter = 0
-
-                        # initialize edge_list
-                        edge_list = []
-
-                    else:
-                        # get the new stroke group
-                        new_stroke = flow_group_counter
-
-                        # add the new stroke to the dictionary
-                        flow_group_dict[new_stroke] = edge_list
-
-                        length_stroke = length_stroke - (length_counter + edge_length)
-                
-                        # extend flow group counter
-                        flow_group_counter += 1
-
-                        # reset the length counter
-                        length_counter = 0
-
-                        # initialize edge_list
-                        edge_list = []
-                
-                
-                # get the edge length
-                length_counter += edge_length
-
-                edge_list.append(index)
-            
-            # get the new stroke group
-            new_stroke = flow_group_counter
-
-            # add the new stroke to the dictionary
-            flow_group_dict[new_stroke] = edge_list
-    
-            # extend flow group counter
-            flow_group_counter += 1
-        else:
-            edge_list = []
-            for index, edge in stroke_gdf.iterrows():
-                edge_list.append(index)
-
-            # get the new stroke group
-            new_stroke = flow_group_counter
-
-            # add the new stroke to the dictionary
-            flow_group_dict[new_stroke] = edge_list
-
-            # extend flow group counter
-            flow_group_counter += 1       
-
-    # edges_gdf['flow_group'] = np.zeros(len(edges_gdf), dtype=np.int64)
-    edges_gdf['flow_group'] = np.ones(len(edges_gdf), dtype=np.int64)*(-1)
-
-    # loop through dictionary
-    for key in flow_group_dict.keys():
-        # get the list of edges
-        edge_list = flow_group_dict[key]
-
-        # loop through the edges in the list
-        for index in edge_list:
-            # add the flow group to the dataframe
-            edges_gdf.loc[index, 'flow_group'] = int(key)
-
-    # # get minium value of flow group
-    # min_flow_group = np.min(edges_gdf['flow_group'].to_numpy())
-    # print(min_flow_group)
-    return edges_gdf
 
 if __name__ == '__main__':
     main()
